dc42b4ce0f0f5642d3702747e94da50772146a96
Sonu Kumar <sonuhits047@gmail.com>
2024-05-02 01:28:31 +0530

    hello world and space sequence

---
+++ day1/p1.py
@@ -0,0 +1,4 @@
+print("Hello World")
+print(5)
+print("Bye")
+print(23*10)
---
+++ day1/p2.py
@@ -0,0 +1,16 @@
+# Space sequence character
+print("I am a good boy\nAnd She is a bad girl.")
+
+'''
+This is our
+multiline comment
+'''
+
+# space sequence character
+print("Hey I am a \" good boy\"\and she is a bad girl")
+
+print("hey", 6, 7)
+
+# it will seperate with tild
+print("hey", 6, 7, sep="~", end="009\n")
+print("Sonu")
